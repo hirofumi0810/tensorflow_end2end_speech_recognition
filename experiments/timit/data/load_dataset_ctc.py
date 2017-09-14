5006fc7865c6612e47e266ba1627c98f546a6380
Lu Huang <huanglu.th@gmail.com>
2017-09-14 12:26:40 +0800

    Revert "Merge branch 'my_master' into master"
    This reverts commit 01c33cc6cec05c5811f0c1d0c595e3869e448d91, reversing
    changes made to 00402111181d65a9621dd5698632d4c6ff5b8a76.

--- experiments/timit/data/load_dataset_ctc.py
+++ experiments/timit/data/load_dataset_ctc.py
@@ -55,9 +55,9 @@ class Dataset(DatasetBase):
         self.progressbar = progressbar
 
         input_path = join(
-            '/home/huanglu/asr/end2end/data/timit/dataset/inputs', data_type)
+            '/n/sd8/inaguma/corpus/timit/dataset/inputs', data_type)
         label_path = join(
-            '/home/huanglu/asr/end2end/data/timit/dataset/labels/ctc',
+            '/n/sd8/inaguma/corpus/timit/dataset/labels/ctc',
             label_type, data_type)
 
         # Load the frame number dictionary
--- experiments/timit/training/run_ctc.sh
+++ experiments/timit/training/run_ctc.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
-MODEL_SAVE_PATH="/home/huanglu/asr/end2end/tf_end2end/experiments/timit/result"
+MODEL_SAVE_PATH="/n/sd8/inaguma/result/timit/"
 
 # Select GPU
 if [ $# -ne 2 ]; then
@@ -14,7 +14,7 @@ export PATH=$PATH:/usr/local/cuda-8.0/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-8.0/lib64:/usr/local/cuda-8.0/extras/CUPTI/lib64
 
 # Set path to python
-PYTHON=/home/huanglu/usr/anaconda2/bin/python
+PYTHON=/home/lab5/inaguma/.pyenv/versions/anaconda3-4.1.1/bin/python
 
 config_path=$1
 gpu_index=$2
